fb48382833e8fe134a5a05b31b01581cf765c431
farzan <farzanullah07@gmail.com>
2024-04-28 16:41:52 +0530

    ready for deployment

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
